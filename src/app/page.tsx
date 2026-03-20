9d09a632ab55a957a9ca1b55ed15270e722b7bda
Federico S. Lanaza <federico.lanaza@neu.edu.ph>
2026-03-20 07:22:11 +0000

    Try fixing this error: `Runtime ReferenceError: cn is not defined. Error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 
 import { useState } from 'react';
@@ -15,6 +14,7 @@ import { useFirestore, useAuth } from '@/firebase';
 import { doc } from 'firebase/firestore';
 import { signInAnonymously } from 'firebase/auth';
 import { setDocumentNonBlocking } from '@/firebase/non-blocking-updates';
+import { cn } from '@/lib/utils';
 
 const ADMIN_AVATAR = "https://chat.google.com/u/0/api/get_attachment_url?url_type=FIFE_URL&content_type=image%2Fjpeg&attachment_token=AOo0EEWNH4d%2BgAqtae6uoEXheU79rwd6rBOVlD54UPphj3A3x2iXPw%2Bek2%2B7K1Ofp80Y8UpZXIzSY4nNoWRRco3jrzSf4vnQMzQXRStjnrvGUAbGqrwlSDSpWE%2FjGeffaFO6gf%2B8ujqEptY8ARR%2FLqCuMYaT9%2FauIeUmelc%2B%2B0tzXtvmJhr9T4pxjxBrV1n3xDdDoQpjlUQ4eRE5KGe6BziK5JgXI3Vo0HthYGfMqrXdtc%2BTvlsraxU0kWr45%2FP3NT40lwY5KZ2Enqh8SB0mILCKg%2Bo0kvx%2FfjJQxrRbr%2BTS%2FMDtxbRGFp%2FfWN%2Fp96qWgqc6y1nWWThZTZ9XbQw3wGBsPP7lg3VS63h5GO1Njl76umXDkKTwG4oWD6wqE3SapynJ6PODXuDLMDY%2BDqMm13qs42tboCMhM5SJHnxq1p5IfqNNuE7hmCTAEURJKMuDqjtZFn2NFCaqsP9I0olJmadSK1Okl5cqJqGBLWpkx6txLMszdDcF%2Fs8Ix%2Fh2Qd1jsyavgUT0iDG47r6UpcY0NHlCrY3JTvZHEDeFYh66YKEis%2BxLDZqUpYgkEmHDlef8PTKepJve0ARYeozIvVUdCsRW&allow_caching=true&sz=w1920-h945-rw&auditContext=forDisplay";
 
